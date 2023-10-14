3cd9985b72bcb9064591364258437c45430ffc0b
Amer alghamdi <akom707@gmail.com>
2023-10-14 21:09:21 +0300

    "bug fix"

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'manualEditor')));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
